c52d6b15441f84c217cd9985dad4904456bf04ba
Gabriel P. B. Piazza <pedro.piazza99@gmail.com>
2023-09-18 09:21:16 -0300

    feat: add threadpool partner into calculation area into shoelace algorithm

--- courses/MultithreadingProgrammingWithGo/threadpool/shoelace.go
+++ courses/MultithreadingProgrammingWithGo/threadpool/shoelace.go
@@ -2,9 +2,15 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"math"
+	"os"
+	"path/filepath"
 	"regexp"
 	"strconv"
+	"strings"
+	"sync"
+	"time"
 )
 
 type Point2D struct {
@@ -12,32 +18,94 @@ type Point2D struct {
 	y int
 }
 
-var (
-	r = regexp.MustCompile(`\((\d*),(\d*)\)`)
+const (
+	numberOfThreads = 5
 )
 
-func calcArea(pointsStr string) float64 {
-	var points []Point2D
+// With amount of work constant and change the number ot threads will not result in a linear performance gain
+// As said in Amdahl's law.
+// Processing took: 563.942166ms 17 number of threads
+// Processing took: 566.587375ms 16 number of threads
+// Processing took: 659.999166ms 15 number of threads
+// Processing took: 690.881208ms 10 number of threads
+// Processing took: 1.004313209s 9 number of threads
+// Processing took: 866.354625ms 8 number of threads
+// Processing took: 840.43575ms  7 number of threads
+// Processing took: 638.301875ms 6 number of threads
+// Processing took: 892.49325m   5 number of threads
+// Processing took: 716.986125ms 4 number of threads
+// Processing took: 930.539667ms 3 number of threads
+// Processing took: 805.170334ms 2 number of threads
+// Processing took: 1.52431325s  1 number of threads
 
-	for _, point := range r.FindAllStringSubmatch(pointsStr, -1) {
-		x, _ := strconv.Atoi(point[1])
-		y, _ := strconv.Atoi(point[2])
-		points = append(points, Point2D{x: x, y: y})
+// We gonna have a pool/queu of itens to process with 1000 spaces, translated into
+// conexationsPool variable, each worker will be connected with the pool and will process each request.
+// We will have 10 woekers, translated in the const numberOfThreads
+
+var (
+	r           = regexp.MustCompile(`\((\d*),(\d*)\)`)
+	waitGroup   = sync.WaitGroup{}
+	locker      = sync.Mutex{}
+	areasResult []float64
+)
+
+func calcArea(inputChan chan string) {
+	for pointsStr := range inputChan {
+		var points []Point2D
+
+		for _, point := range r.FindAllStringSubmatch(pointsStr, -1) {
+			x, _ := strconv.Atoi(point[1])
+			y, _ := strconv.Atoi(point[2])
+			points = append(points, Point2D{x: x, y: y})
+		}
+
+		var area float64
+		for i := 0; i < len(points); i++ {
+			neverWillBetOutOfRange := (i + 1) % len(points)
+			a, b := points[i], points[neverWillBetOutOfRange]
+			area += float64(a.x*b.y) - float64(a.y*b.x)
+		}
+
+		area = math.Abs(area) / 2
+		fmt.Println("Polygon area:", area)
+		locker.Lock()
+		areasResult = append(areasResult, area)
+		locker.Unlock()
 	}
-
-	var area float64
-	for i := 0; i < len(points); i++ {
-		neverWillBetOutOfRange := (i + 1) % len(points)
-		a, b := points[i], points[neverWillBetOutOfRange]
-		area += float64(a.x*b.y) - float64(a.y*b.x)
-	}
-
-	area = math.Abs(area) / 2
-	return area
+	waitGroup.Done()
 }
 
 func main() {
-	line := "(4,10),(12,8),(10,3),(2,2),(7,5)"
-	area := calcArea(line)
-	fmt.Println("Polygon area:", area)
+	absPaht, err := filepath.Abs("./polygons.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	dat, err := os.ReadFile(absPaht)
+	if err != nil {
+		log.Fatal(err)
+	}
+	polyTxt := string(dat)
+
+	start := time.Now()
+	masterInitWorker(polyTxt)
+	end := time.Since(start)
+	fmt.Println("Processing took:", end)
+	// fmt.Println("Polygons areas:", areasResult)
+}
+
+func masterInitWorker(polyTxt string) {
+	conexationsPool := make(chan string, 1000)
+	initWorkers(conexationsPool)
+	for _, line := range strings.Split(polyTxt, "\n") {
+		conexationsPool <- line
+	}
+	close(conexationsPool)
+	waitGroup.Wait()
+}
+
+func initWorkers(conexationsPool chan string) {
+	for i := 0; i < numberOfThreads; i++ {
+		go calcArea(conexationsPool)
+	}
+	waitGroup.Add(numberOfThreads)
 }
