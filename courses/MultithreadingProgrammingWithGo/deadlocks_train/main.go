0bd7c934151ed75418b2ea9b0ab20e1ad89e721d
Gabriel P. B. Piazza <pedro.piazza99@gmail.com>
2023-09-21 09:09:20 -0300

    feat: implement trains init

--- courses/MultithreadingProgrammingWithGo/deadlocks_train/main.go
+++ courses/MultithreadingProgrammingWithGo/deadlocks_train/main.go
@@ -19,10 +19,35 @@ func main() {
 		intersections[i] = NewIntersection(i)
 	}
 
+	initTrains()
+
 	s := NewSimulation()
 	ebiten.SetWindowSize(s.Layout(0, 0))
 	ebiten.SetWindowTitle("Trains simulation")
 	if err := ebiten.RunGame(s); err != nil {
 		log.Fatal(err)
 	}
+}
+
+func initTrains() {
+	go MoveTrain(trains[0], 300,
+		[]*Crossing{
+			{Position: 125, Intersection: intersections[0]},
+			{Position: 175, Intersection: intersections[1]},
+		})
+
+	go MoveTrain(trains[1], 300, []*Crossing{
+		{Position: 125, Intersection: intersections[1]},
+		{Position: 175, Intersection: intersections[2]},
+	})
+
+	go MoveTrain(trains[2], 300, []*Crossing{
+		{Position: 125, Intersection: intersections[2]},
+		{Position: 175, Intersection: intersections[3]},
+	})
+
+	go MoveTrain(trains[3], 300, []*Crossing{
+		{Position: 125, Intersection: intersections[3]},
+		{Position: 175, Intersection: intersections[0]},
+	})
 }
