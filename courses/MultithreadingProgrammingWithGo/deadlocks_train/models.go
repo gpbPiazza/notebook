ffdeb95322bdcf331387f3c92f781abca315885c
Gabriel P. B. Piazza <pedro.piazza99@gmail.com>
2023-10-02 08:53:14 -0300

    feat: add semaphore solution to train deadlocks examples

--- courses/MultithreadingProgrammingWithGo/deadlocks_train/models.go
+++ courses/MultithreadingProgrammingWithGo/deadlocks_train/models.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"sort"
 	"sync"
 	"time"
 )
@@ -33,8 +34,29 @@ func NewIntersection(id int) *Intersection {
 }
 
 type Crossing struct {
+	PriorityLock int
 	Position     int
 	Intersection *Intersection
+}
+
+func lockIntersectionsByPriority(id, reversedStart, reversedEnd int, crossings []*Crossing) {
+	var intersectionsToLock []*Intersection
+	for _, c := range crossings {
+		if reversedEnd >= c.Position &&
+			reversedStart <= c.Position &&
+			c.Intersection.LockedBy != id {
+			intersectionsToLock = append(intersectionsToLock, c.Intersection)
+		}
+	}
+
+	sort.Slice(intersectionsToLock, func(i, j int) bool {
+		return intersectionsToLock[i].ID > intersectionsToLock[j].ID
+	})
+
+	for _, it := range intersectionsToLock {
+		it.Mutex.Lock()
+		it.LockedBy = id
+	}
 }
 
 func MoveTrain(train *Train, distance int, crossings []*Crossing) {
@@ -42,8 +64,9 @@ func MoveTrain(train *Train, distance int, crossings []*Crossing) {
 		train.Front += 1
 		for _, cross := range crossings {
 			if train.Front == cross.Position {
-				cross.Intersection.Mutex.Lock()
-				cross.Intersection.LockedBy = train.ID
+				reversedStart := cross.Position
+				reversedEnd := cross.Position + trainLenght
+				lockIntersectionsByPriority(train.ID, reversedStart, reversedEnd, crossings)
 			}
 			back := train.Front - train.Length
 			if back == cross.Position {
