ef968b5379fcc52f0ed6ffc71368032e6a684d19
Gabriel P. B. Piazza <pedro.piazza99@gmail.com>
2024-03-06 07:51:28 -0300

    chore: add more test codes

--- codes/ctx_collision_key.go
+++ codes/ctx_collision_key.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"context"
-	"fmt"
 	"log"
 )
 
@@ -35,23 +34,23 @@ func Conn(ctx context.Context) *MyConnectionPool {
 
 // Aprendizado, quando você sobreescreve uma chave no ctx ja existente
 // quando você consultar essa chave você sempre irá receber o último valor setada, como uma pilha.
-func main() {
-	ctx := context.Background()
+// func main() {
+// 	ctx := context.Background()
 
-	for i := 0; i < 15; i++ {
-		gremio := new(MyConnectionPool)
-		gremio.DbName = fmt.Sprintf("DD%d", i)
-		gremio.Number = i
-		ctx = SetConnCtx(ctx, gremio)
-		meuGremio := Conn(ctx)
+// 	for i := 0; i < 15; i++ {
+// 		gremio := new(MyConnectionPool)
+// 		gremio.DbName = fmt.Sprintf("DD%d", i)
+// 		gremio.Number = i
+// 		ctx = SetConnCtx(ctx, gremio)
+// 		meuGremio := Conn(ctx)
 
-		if meuGremio.DbName != gremio.DbName {
-			fmt.Println("OPA NÃO FUNCINOU COMO DIZ NO COMENTÁRIO")
-		}
-		if meuGremio.Number != gremio.Number {
-			fmt.Println("OPA NÃO FUNCINOU COMO DIZ NO COMENTÁRIO")
-		}
-	}
+// 		if meuGremio.DbName != gremio.DbName {
+// 			fmt.Println("OPA NÃO FUNCINOU COMO DIZ NO COMENTÁRIO")
+// 		}
+// 		if meuGremio.Number != gremio.Number {
+// 			fmt.Println("OPA NÃO FUNCINOU COMO DIZ NO COMENTÁRIO")
+// 		}
+// 	}
 
-	fmt.Println("OPA FUNCINOU COMO DIZ NO COMENTÁRIO")
-}
+// 	fmt.Println("OPA FUNCINOU COMO DIZ NO COMENTÁRIO")
+// }
---
+++ codes/wait_group_duplicated.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+type httpPkg struct{}
+
+func (httpPkg) Get(url string) {}
+
+var http httpPkg
+
+func waitGroup1() {
+	var wg sync.WaitGroup
+	fmt.Println("waitGroup 1, valor ->", wg)
+	for i := range []int{1, 2, 3, 4} {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			fmt.Println("waitGroup 1, goroutine ->", i)
+			time.Sleep(1 * time.Second)
+		}()
+	}
+	fmt.Println("waitGroup 1, waiting for all routines end")
+	wg.Wait()
+	fmt.Println("waitGroup 1, finish")
+}
+
+func waitGroup2() {
+	var wg sync.WaitGroup
+	fmt.Println("waitGroup 2, valor ->", wg)
+	for i := range []int{1, 2, 3, 4} {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			fmt.Println("waitGroup 2, goroutine ->", i)
+			time.Sleep(1 * time.Second)
+		}()
+	}
+	fmt.Println("waitGroup 2, waiting for all routines end")
+	wg.Wait()
+	fmt.Println("waitGroup 2, finish")
+}
+
+func main() {
+	fmt.Println("waitGroup FATHER start")
+	var wg sync.WaitGroup
+	wg.Add(1)
+	go waitGroup1()
+	wg.Add(1)
+	go waitGroup2()
+	fmt.Println("waitGroup FATHER waiting")
+	wg.Wait()
+	fmt.Println("waitGroup FATHER finish")
+}
