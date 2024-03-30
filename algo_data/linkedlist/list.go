2d2b1453ef905ecd0e5a2f8f9f30aaa5fc8d9f81
Gabriel P. B. Piazza <pedro.piazza99@gmail.com>
2024-03-30 11:10:04 -0300

    feat: implement New list, SliceToList, Add, FindMiddleNode and DeleteMiddleNode methods

--- algo_data/linkedlist/list.go
+++ algo_data/linkedlist/list.go
@@ -1,1 +1,76 @@
 package linkedlist
+
+type Node struct {
+	Val  int
+	Next *Node
+}
+
+func New(val int) *Node {
+	return &Node{
+		Val: val,
+	}
+}
+
+// SliceToList uses the inputed slice to build a new linkedList
+// SliceToList returns the head of the linkedList
+func SliceToList(vals []int) *Node {
+	if len(vals) == 0 {
+		return nil
+	}
+
+	head := &Node{
+		Val: vals[0],
+	}
+	for i := 1; i < len(vals); i++ {
+		head.Add(vals[i])
+	}
+	return head
+}
+
+// Add concat new Node to the last node in the list and return the head of the list
+func (l *Node) Add(val int) *Node {
+	newNode := &Node{Val: val}
+	iterator := l
+	for iterator.Next != nil {
+		iterator = iterator.Next
+	}
+	iterator.Next = newNode
+	return l
+}
+
+func (l *Node) FindMiddleNode() *Node {
+	endNode := l
+	middleNode := l
+	for endNode != nil && endNode.Next != nil {
+		endNode = endNode.Next.Next
+		middleNode = middleNode.Next
+	}
+
+	return middleNode
+}
+
+func (l *Node) DeleteMiddleNode() *Node {
+	endNode := l
+	middleIndex := 0
+	for endNode != nil && endNode.Next != nil {
+		endNode = endNode.Next.Next
+		middleIndex++
+	}
+
+	if middleIndex == 0 {
+		l.Next = nil
+		return l
+	}
+
+	preivousIndexFromMiddle := middleIndex - 1
+
+	iterator := l
+	index := 0
+	for index < preivousIndexFromMiddle {
+		iterator = iterator.Next
+		index++
+	}
+	iterator.Next = iterator.Next.Next
+
+	return l
+}
---
+++ algo_data/linkedlist/list_test.go
@@ -0,0 +1,194 @@
+package linkedlist
+
+import (
+	"reflect"
+	"testing"
+)
+
+func Test_New(t *testing.T) {
+	type testCase struct {
+		name  string
+		input int
+		want  *Node
+	}
+
+	tests := []testCase{
+		{
+			name:  "should return new pointer to new list with val int",
+			input: 10,
+			want:  &Node{Val: 10},
+		},
+		{
+			name: "should return empty head where input is empty",
+			want: &Node{},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := New(tt.input)
+			if !reflect.DeepEqual(*tt.want, *got) {
+				t.Errorf("%s - got: %v want: %v", tt.name, *got, *tt.want)
+			}
+		})
+	}
+
+}
+
+func Test_SliceToList(t *testing.T) {
+	type testCase struct {
+		name  string
+		input []int
+		want  *Node
+	}
+
+	tests := []testCase{
+		{
+			name:  "should return list poiting in the same order as the slice",
+			input: []int{10, 2, 3},
+			want:  &Node{Val: 10, Next: &Node{Val: 2, Next: &Node{Val: 3}}},
+		},
+		{
+			name:  "should return list poiting to only on node",
+			input: []int{10},
+			want:  &Node{Val: 10},
+		},
+		{
+			name:  "should return nil if input is nil",
+			input: nil,
+			want:  nil,
+		},
+		{
+			name:  "should return nil if input has zero len",
+			input: []int{},
+			want:  nil,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := SliceToList(tt.input)
+			if got != nil && tt.want != nil && !reflect.DeepEqual(*tt.want, *got) {
+				t.Errorf("%s - got: %v want: %v", tt.name, *got, *tt.want)
+			}
+		})
+	}
+
+}
+
+func Test_FindMiddleNode(t *testing.T) {
+	type testCase struct {
+		name      string
+		want      *Node
+		buildList func() *Node
+	}
+
+	cases := []testCase{
+		{
+			name: "should return the first node when list only have one node",
+			want: &Node{Val: 10},
+			buildList: func() *Node {
+				return &Node{Val: 10}
+			},
+		},
+		{
+			name: "should return empty Node if the list is empty",
+			want: &Node{},
+			buildList: func() *Node {
+				return &Node{}
+			},
+		},
+		{
+			name: "should return the middle node (index 1) in the list of 3 elements",
+			want: &Node{Val: 2, Next: &Node{Val: 3}},
+			buildList: func() *Node {
+				return &Node{Val: 10, Next: &Node{Val: 2, Next: &Node{Val: 3}}}
+			},
+		},
+		{
+			name: "should return the middle node (index 3) in the list of 6 elements",
+			want: &Node{
+				Val: 4,
+				Next: &Node{
+					Val: 5,
+					Next: &Node{
+						Val: 6,
+					},
+				},
+			},
+			buildList: func() *Node {
+				return &Node{
+					Val: 1,
+					Next: &Node{
+						Val: 2,
+						Next: &Node{
+							Val: 3,
+							Next: &Node{
+								Val: 4,
+								Next: &Node{
+									Val: 5,
+									Next: &Node{
+										Val: 6,
+									},
+								},
+							},
+						},
+					},
+				}
+			},
+		},
+	}
+	for _, tt := range cases {
+		t.Run(tt.name, func(t *testing.T) {
+			middleNode := tt.buildList().FindMiddleNode()
+			if middleNode != nil && tt.want != nil && !reflect.DeepEqual(*tt.want, *middleNode) {
+				t.Errorf("%s - want: %v - got: %v", tt.name, *tt.want, *middleNode)
+			}
+		})
+	}
+}
+
+func Test_DeleteMiddleNode(t *testing.T) {
+	type testCase struct {
+		name      string
+		want      *Node
+		buildList func() *Node
+	}
+
+	cases := []testCase{
+		{
+			name: "should return first node when has two elements",
+			want: &Node{Val: 1},
+			buildList: func() *Node {
+				return &Node{Val: 1, Next: &Node{Val: 2}}
+			},
+		},
+		{
+			name: "should return the list without the node with val 7",
+			// [1,3,4,1,2,6]
+			want: &Node{Val: 1, Next: &Node{Val: 3, Next: &Node{Val: 4, Next: &Node{Val: 1, Next: &Node{Val: 2, Next: &Node{Val: 6}}}}}},
+			// [1,3,4,7,1,2,6]
+			buildList: func() *Node {
+				return &Node{Val: 1, Next: &Node{Val: 3, Next: &Node{Val: 4, Next: &Node{Val: 7, Next: &Node{Val: 1, Next: &Node{Val: 2, Next: &Node{Val: 6}}}}}}}
+			},
+		},
+		{
+			name: "should return the list without the node with val 3",
+			// [1,2,4]
+			want: &Node{Val: 1, Next: &Node{Val: 2, Next: &Node{Val: 4}}},
+			// [1,2,3,4]
+			buildList: func() *Node {
+				return &Node{Val: 1, Next: &Node{Val: 2, Next: &Node{Val: 3, Next: &Node{Val: 4}}}}
+			},
+		},
+	}
+	for _, tt := range cases {
+		t.Run(tt.name, func(t *testing.T) {
+			got := tt.buildList().DeleteMiddleNode()
+
+			if !reflect.DeepEqual(tt.want, got) {
+				t.Errorf("%s - want: %v got: %v", tt.name, tt.want, got)
+			}
+		})
+	}
+}
