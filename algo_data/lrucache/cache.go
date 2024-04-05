88264f1f4d8a018a849a94a5a00c488d7c86a271
Gabriel P. B. Piazza <pedro.piazza99@gmail.com>
2024-04-05 08:58:23 -0300

    re-impolement lru

--- algo_data/lrucache/cache.go
+++ algo_data/lrucache/cache.go
@@ -1,86 +1,79 @@
 package lrucache
 
-// this LRU cache are using Least Recently Used
-// if get a data he is the most recently used
-// if data is updated or inserted he is the most recently used
 type Node struct {
 	Key  int
 	Val  int
-	Next *Node
 	Prev *Node
+	Next *Node
 }
 
-func NewNode(key int, val int) *Node {
-	return &Node{
-		Key:  key,
-		Val:  val,
-		Next: nil,
-		Prev: nil,
-	}
+func NewNode(key, val int) *Node {
+	return &Node{Val: val, Key: key}
 }
 
 type LRUCache struct {
 	capacity int
-	right    *Node // mRU - most recently used
-	left     *Node // lRU - last recently used
-	cache    map[int]*Node
+
+	cache map[int]*Node
+	left  *Node // left.Next = LRU
+	right *Node // right.Previus = MRU
 }
 
 func Constructor(capacity int) LRUCache {
 	left := NewNode(0, 0)
 	right := NewNode(0, 0)
-	right.Prev = left
+
 	left.Next = right
+	right.Prev = left
 	return LRUCache{
 		capacity: capacity,
-		right:    right,
 		left:     left,
+		right:    right,
 		cache:    make(map[int]*Node),
 	}
 }
 
 func (lru *LRUCache) Put(key int, data int) {
-	node, exist := lru.cache[key]
-	newNode := NewNode(key, data)
-	if exist {
-		lru.removeFromList(node)
+	node, exists := lru.cache[key]
+	if exists {
+		lru.removeNodeFromList(node)
 	}
+	newNode := NewNode(key, data)
 
+	lru.insertAtRight(newNode)
 	lru.cache[key] = newNode
-	lru.insertAtRight(lru.cache[key])
 
-	if lru.capacity < len(lru.cache) {
-		// remove the last recently used from the list and from the cache
+	if len(lru.cache) > lru.capacity {
 		lruElement := lru.left.Next
-		lru.removeFromList(lruElement)
+		lru.removeNodeFromList(lruElement)
 		delete(lru.cache, lruElement.Key)
 	}
 }
 
-func (lru *LRUCache) Get(key int) int {
-	node, exists := lru.cache[key]
-	if exists {
-		lru.makeNodeMRU(node)
-		return node.Val
-	}
-
-	return -1
-}
-func (lru *LRUCache) makeNodeMRU(node *Node) {
-	lru.removeFromList(node)
-	lru.insertAtRight(node)
-}
-
-func (lru *LRUCache) removeFromList(node *Node) {
+func (lru *LRUCache) removeNodeFromList(node *Node) {
 	prev, next := node.Prev, node.Next
+
 	prev.Next = next
 	next.Prev = prev
 }
 
-func (lru *LRUCache) insertAtRight(node *Node) {
-	prev := lru.right.Prev
-	lru.right.Prev = node
-	prev.Next = node
-	node.Next = lru.right
-	node.Prev = prev
+func (lru *LRUCache) insertAtRight(newNode *Node) {
+	lastMRU := lru.right.Prev
+
+	lru.right.Prev = newNode
+	newNode.Next = lru.right
+	lastMRU.Next = newNode
+	newNode.Prev = lastMRU
+}
+
+func (lru *LRUCache) Get(key int) int {
+	node, exists := lru.cache[key]
+	if !exists {
+		return -1
+	}
+
+	lru.removeNodeFromList(node)
+	lru.insertAtRight(node)
+
+	return node.Val
 }
