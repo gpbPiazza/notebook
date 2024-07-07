4b3ebfd80dbd2d689ff11bff0b284c3e54f39493
Gabriel P. B. Piazza <pedro.piazza99@gmail.com>
2024-07-07 09:53:00 -0300

    chore: done poc to discover how to test package dependency architecture test

--- my_go_playground/playground/build_import_pahts.go
+++ my_go_playground/playground/build_import_pahts.go
@@ -8,10 +8,19 @@ import (
 
 func TestImportPaths() {
 
-	buildPkg, err := build.Import("github.com/gpbPiazza/notebook/algo_data/queues", "", build.IgnoreVendor)
-	if err != nil {
-		log.Fatal(err.Error())
+	notAllowedDependOn := map[string]bool{
+		"github.com/gpbPiazza/notebook/algo_data/queues": true,
 	}
 
-	fmt.Println(buildPkg.Imports)
+	buildPkg, err := build.Import("github.com/gpbPiazza/notebook/algo_data/graphs", "", build.ImportComment)
+	if err != nil {
+		log.Fatalf("Failed to import package: %v", err)
+	}
+
+	for _, imp := range buildPkg.Imports {
+		if notAllowedDependOn[imp] {
+			log.Fatalf("It is not allowed to graphs package depend on %v package", imp)
+		}
+		fmt.Printf("Imported package: %s\n", imp)
+	}
 }
